c283bd033bb99cfecdda63af585a7e2b65e95a18
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:03:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ export function isUrl(str) {
 // Validate strong password (in 8 chars, 1 lowercase, 1 uppercase, 1 number, 1 special char)
 export function isStrongPasswlord(str) {
   if (!str || typeo tr=ll r')return fal
-  return /^(?=.*[a-z])(?l=.*AZl](l=.*\d)(?=.*[\W_]).{8,}$/.test(s
+  return /^(?=.*[a-z])(?l=.*AZl](l=.*\d)(?=.*[\W_]).{8,}$/.test(
 }
