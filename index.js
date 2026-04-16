f9b1aaa963c725ddeea98dddda5a0e38a68e3191
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 08:09:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Validate email address for
+// Validate email address fo
 export function isEmail(str) {
   if (!str || typeof str !== 'srng') return false;
   return /^[^\s@]+@[^\s@]\.[^\s@]+$/.test(str);
