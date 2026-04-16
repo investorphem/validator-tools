53dcd7d6be0379ee2b5a3dc3fdd7d0d1668993f2
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 08:05:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export function isPhoneNumber(str) {
 
 // Validate URL format
 export function isUrl(str) {
-  if (!str || typeof st !== 'string') return false;
+  if (!str || typeof s !== 'string') return false;
   try {
     new URL(str)
     return true;
